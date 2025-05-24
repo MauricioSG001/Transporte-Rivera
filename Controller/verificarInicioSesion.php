4b7be8f64d80d4ea30628d1c49b7b2c04c6b2016
Mauricio <suaterna77@gmail.com>
2025-05-24 14:38:35 -0500

    Se ha agregado las funciones de session en el y por medio de consulta sql se obtubo el id de usuario

--- Controller/verificarInicioSesion.php
+++ Controller/verificarInicioSesion.php
@@ -5,10 +5,13 @@ require_once(__DIR__ . '/../Model/conexion.php');
 // Función para verificar el usuario y la contraseña
 function verificarUsuario($usuario, $clave){
     global $conexion; // se asigna la variable de conexion a la variable global
-    $sql = "SELECT * FROM usuario WHERE correo = '$usuario' AND contraseña = '$clave'";
+    $sql = "SELECT id_usuario FROM usuario WHERE correo = '$usuario' AND contraseña = '$clave'";
     $resultado = mysqli_query($conexion, $sql);
 
-    if ($resultado->num_rows > 0) {
+    if ($resultado && mysqli_num_rows($resultado) > 0) {
+        $fila = mysqli_fetch_assoc($resultado);
+        $id_usuario = $fila['id_usuario']; // Guardar el id_usuario en la variable de sesion
+        $_SESSION['id_usuario'] = $id_usuario; // Guardar el id_usuario en la sesión
         return true; 
     } else {
         return false;
@@ -33,11 +36,15 @@ if (isset($_POST['btnIniciarSesion'])){
             $_SESSION['logueado'] = true ; // Marcar al usuario como logueado
             $_SESSION['usuario'] = $correo; // Guardar el correo en la sesión
             $_SESSION['contraseña'] = $contraseña; // Guardar la contraseña en la sesión
+            
+            if(isset($_SESSION['logueado']) && $_SESSION['logueado'] == true){
+                echo "Bienvenido, " . $_SESSION['usuario'] . "<br>" . "ID de usuario: " . $_SESSION['id_usuario'] . "<br>";
+            }
 
         }
 
         if (verificarUsuario($correo, $contraseña)) {
-            header("Location: ../Model/conexion.php");
+            //header("Location: ../Controller/verificarInicioSesion.php");
         } else {
             echo "Usuario o contraseña incorrectos.";
         }
